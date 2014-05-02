e5daf26f52da34bc5f569fed0c9fc3d11f936a72
troutbum <genec2008@gmail.com>
2014-05-01 21:54:00 -0700

    Closer
    need to debug from calling levels

--- rankall.R
+++ rankall.R
@@ -28,9 +28,10 @@ rankall <- function(outcome, num) {
         ##
         alldata <- read.csv("outcome-of-care-measures.csv",     # Data import problem resolved
                             colClasses = "character")           # by coercing to "character"                    
-     
+        
         # Condense data to just hospital, mortality rate and state
-        datacol <- as.numeric(subset(alldata)[,j])                      # extract mortality rate column 
+        datacol <- suppressWarnings(as.numeric(subset(alldata)[,j]))    # extract mortality rate column non-numeric 
+        # data  (e.g. 'Not Available') coerced to NAs
         namecol <- as.character(subset(alldata)[,"Hospital.Name"])      # extract hospital name column
         statecol <- as.character(subset(alldata)[,"State"])             # extract State name                
         
@@ -62,65 +63,34 @@ rankall <- function(outcome, num) {
                                StateName=character(),
                                stringsAsFactors=FALSE) 
         
-        for (i in 1:nStates) {                                          # iterate through each state
+        # Iterate through all states
+        for (i in 1:nStates) {  
+                
+                # Set rank based upon NUM input
+                # If NUM == a character vector
+                if (is.character(num)) {                                        
+                        if (num == "best") {
+                                rank <- 1                               # Best hospital in rankings
+                        }           
+                        if (num == "worst") {                           # Last hospital in rankings
+                                rank <- as.numeric(nRanked[i])
+                        }
+                        else {
+                                return("invalid input characters in num argument")
+                        }           
+                }        
+                # If NUM == a numeric vector
+                if (is.numeric(num)) {                                          
+                        rank <- as.integer(num)                                                   
+                }
+                
                 stateRankings <- rankings[[stateCodes[i]]]              # to make ranked list of hospitals
                 stateHospital <- as.character(stateRankings[rank,1])    # pick hospital based on input NUM
                 outputDF[i,"HospitalName"] <- stateHospital             # stuff results into data.frame
                 outputDF[i,"StateName"] <- stateCodes[i]               
-        }
-
-        
-        
-        # If NUM == a character vector
-        #
-        # Return hospital name (character vector) that has the ranking specified by the num argument.  
-        #
-        if (is.character(num)) {
-                
-                if (num == "best") {
-                        hospitalName <- as.character(sortedDF[1,1])             # return top of list
-                        return(hospitalName)
-                }           
-                if (num == "worst") {
-                        hospitalName <- as.character(sortedDF[rankings,1])    # return bottom of list
-                        return(hospitalName)
-                }
-                else {
-                        return("invalid input characters in num argument")
-                }           
-        }
-        
-        # If NUM == a numeric vector then
-        #
-        # Return hospital name (character vector) that has the ranking specified by the num argument.  
-        #
-        if (is.numeric(num)) {
-                
-                # test to see if num arg is valid             
-                rank <- as.integer(num)                                 # convert input arg to integer    
-               
-                if (rank > rankings) {                                  # exceeds number of rankings
-                        return("NA") 
-                }
-                
-                hospitalName <- as.character(sortedDF[rank,1])         # return based on requested ranking
-                return(hospitalName)
-                
-        }  
-         
-        # should not end up here with valid NUM input
-        #
-        else {
-                return(":::ERROR:::invalid input for NUM")
-        }
-      
+        } 
+        return(outputDF)
 }
-
-## unused but useful example code
-#
-# rate <- min(statedata[,j])                              # determine lowest mortality
-# i <- which.min(statedata[,j])                           # which row?
-# best_hospital <- as.character(statedata[i,"Hospital.Name"])
 
 
 
